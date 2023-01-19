ef5d3e74ceaedb03d43eb62810339b9a637ae9c8
chris-pitre <c.tophanp3@gmail.com>
2023-01-19 11:33:16 -0500

    created exercise_01

---
+++ exercise_01.py
@@ -0,0 +1,8 @@
+def get_unique_list(dataList):
+    dataList = set(dataList)
+    dataList = list(dataList)
+    return dataList
+
+my_list = [1, 2, 3, 2, 1, 4]
+unique_list = get_unique_list(my_list)
+print(unique_list)
